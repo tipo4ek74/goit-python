d4c5700b02f4dc912c1db98dab65102bcc491a59
tipo4ek <kolesnicka@gmail.com>
2020-12-12 15:56:36 +0200

    hw1

---
+++ lesson1/hw2.py
@@ -0,0 +1,12 @@
+a = float(input("a "))
+b = float(input("b "))
+c = float(input("c "))
+
+D = (b ** 2 - 4 * a * c)
+
+x1 = (-b + D ** 0.5) / 2 * a
+x2 = (-b - D ** 0.5) / 2 * a
+
+print("Дискриминант D : ", + float(D))
+print("x1 = ", float(x1))
+print("x2 = ", float(x2))
